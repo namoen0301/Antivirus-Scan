5d679208f60063c04fa00672ae76d9daa68a67aa
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2025-04-20 01:17:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>杀软在线识别-渊龙Sec安全团队</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="all.min.css">
     <link rel="stylesheet" href="styles.css">
     <script>
         function clearTextarea() {
