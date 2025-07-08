eea2acdf804203d128952896207b4a5525355cf5
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2025-07-08 15:08:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -194,7 +194,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php endif; ?>
         
         <div class="footer">
-            <p><b>项目版本号 V1.8.3-2025.4</b></p>
+            <p><b>项目版本号 V1.8.4-2025.7</b></p>
         </div>
     </div>
 </body>
